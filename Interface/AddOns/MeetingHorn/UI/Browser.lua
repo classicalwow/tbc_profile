2195a9c49ebc03c9797ace1fc63c0466c16efb53
Typescript <poolk008@qq.com>
2022-02-12 17:17:22 +0800

    update

--- Interface/AddOns/MeetingHorn/UI/Browser.lua
+++ Interface/AddOns/MeetingHorn/UI/Browser.lua
@@ -113,12 +113,14 @@ function Browser:Constructor()
     --@end-classic@]=]
 
     -- @bcc@
-    SetupQuickButton(self.Quick1, 3607)
-    SetupQuickButton(self.Quick2, 3845)
-    SetupQuickButton2(self.Quick3, L['5H'])
+    SetupQuickButton(self.Quick2, 3607)
+    SetupQuickButton(self.Quick3, 3845)
+    SetupQuickButton2(self.Quick1, L['5H'])
     SetupQuickButton(self.Quick4, 3457)
     SetupQuickButton(self.Quick5, 3836)
     SetupQuickButton(self.Quick6, 3923)
+    SetupQuickButton(self.Quick7, 3606)
+    SetupQuickButton(self.Quick8, 3959)
     -- @end-bcc@
 
     self.Activity:SetMenuTable(ns.ACTIVITY_FILTER_MENU)
